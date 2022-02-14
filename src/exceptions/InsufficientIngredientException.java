448bd6a48e7a0614cf54db0158dd41670cd01d3d
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-02-14 21:52:56 +0530

    Restaurant Management System

--- src/exceptions/InsufficientIngredientException.java
+++ src/exceptions/InsufficientIngredientException.java
@@ -1,5 +1,18 @@
 package exceptions;
 
-public class InsufficientIngredientException extends RuntimeException{
+import entities.Ingredient;
 
+import java.util.Map;
+
+public class InsufficientIngredientException extends RuntimeException{
+    private Map<Ingredient,Double> insufficientIngredients;
+
+
+    public InsufficientIngredientException(Map<Ingredient, Double> insufficientIngredients) {
+     this.insufficientIngredients = insufficientIngredients;
+    }
+
+    public Map<Ingredient, Double> getInsufficientIngredients() {
+        return insufficientIngredients;
+    }
 }
