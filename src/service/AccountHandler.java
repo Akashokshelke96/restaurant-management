ae2c30556cee677c68152bf9bae1f2c78c97664c
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-02-15 22:06:23 +0530

    Restaurant Management System Final

--- src/service/AccountHandler.java
+++ src/service/AccountHandler.java
@@ -7,17 +7,19 @@ import java.util.List;
 
 public class AccountHandler {
     public void printSales(List<Sales> salesList){
+        System.out.println("Total number of  Sales done so far " + salesList.size());
         for(Sales sales : salesList) {
             System.out.println(sales.toString());
         }
     }
-    public void printExpense(List<Expense> expenseList){
+    public void printExpenses(List<Expense> expenseList){
+        System.out.println("Total number of Expense done so far " + expenseList.size());
         for (Expense expense: expenseList){
             System.out.println(expense.toString());
-
         }
     }
     public void printProfit(List<Sales> salesList, List<Expense> expenseList ){
+        System.out.println("");
         double netProfit = 0;
         for(Sales sales : salesList){
             netProfit += sales.getAmount();
