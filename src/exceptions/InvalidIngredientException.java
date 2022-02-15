feacfbb68df045aa3faa7799a94029d6a7343333
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-02-15 22:09:05 +0530

    Restaurant Management System Final

---
+++ src/exceptions/InvalidIngredientException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class InvalidIngredientException extends RuntimeException{
+    public InvalidIngredientException (String  message){
+        super(message);
+
+    }
+
+}
