805a5ad7cc3d676a48d0df2d86a31b1b569392b3
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-02-15 22:08:32 +0530

    Restaurant Management System Final

--- src/entities/Ingredient.java
+++ src/entities/Ingredient.java
@@ -32,9 +32,25 @@ public class Ingredient {
     public void setRate(double rate) {
         this.rate = rate;
     }
+    @Override
+    public String toString(){
+        return "Name = " + this.name + ", Qty = " + this.qty + ", Rate = " + this.rate;
+
+    }
+    @Override
+    public boolean equals(Object object){
+        if(this.getClass() != object.getClass()){
+            return false;
+        }
+        else { Ingredient otherIngredient = (Ingredient) object;
+            return this.getName().equals(otherIngredient.getName());
+        }
+    }
 
     private double qty;
     private double rate;
 
 }
+
+
 //this class contains an object of ingredient
